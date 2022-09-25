3665117ac0f76e71a25d4f328119c97a6e711419
thomiz <thomasrosenlund@gmail.com>
2022-09-25 19:11:18 +0200

    Try -packages param

--- docker.sh
+++ docker.sh
@@ -5,4 +5,6 @@ pwd
 ls -al /github/workspace/igs/snapshots
 mkdir -p /github/workspace/.fhir/packages
 ls -al /github/workspace/.fhir/packages
+echo "Copy"
 cp -r /github/workspace/igs/snapshots/* /github/workspace/.fhir/packages
+ls -al /github/workspace/.fhir/packages
