78e957a67ac757958efedd48bca6333f558ab210
thomiz <thomasrosenlund@gmail.com>
2022-09-25 19:03:47 +0200

    Try cp from scripted file

--- docker.sh
+++ docker.sh
@@ -5,3 +5,4 @@ pwd
 ls -al /github/workspace/igs/snapshots
 mkdir -p /github/workspace/.fhir/packages
 ls -al /github/workspace/.fhir/packages
+cp -r /github/workspace/igs/snapshots/* /github/workspace/.fhir/packages
