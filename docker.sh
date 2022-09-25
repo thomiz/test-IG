770ebde26ec3982974745a41fca661689766ed66
thomiz <thomasrosenlund@gmail.com>
2022-09-25 19:24:38 +0200

    make directory first

--- docker.sh
+++ docker.sh
@@ -5,6 +5,8 @@ pwd
 ls -al /github/workspace/igs/snapshots
 mkdir -p /github/workspace/.fhir/packages
 ls -al /github/workspace/.fhir/packages
+echo "make directory home publisher fhir packages"
+mkdir -p /home/publisher/.fhir/packages
 echo "Copy"
 cp -r /github/workspace/igs/snapshots/* /home/publisher/.fhir/packages
 echo "home publisher .fhir packages"
