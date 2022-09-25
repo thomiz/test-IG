69acc45a0ad1e55e8eb3eea0faeefed8bae9b657
thomiz <thomasrosenlund@gmail.com>
2022-09-25 19:19:54 +0200

    change the sh script

--- docker.sh
+++ docker.sh
@@ -6,5 +6,6 @@ ls -al /github/workspace/igs/snapshots
 mkdir -p /github/workspace/.fhir/packages
 ls -al /github/workspace/.fhir/packages
 echo "Copy"
-cp -r /github/workspace/igs/snapshots/* /github/workspace/.fhir/packages
-ls -al /github/workspace/.fhir/packages
+cp -r /github/workspace/igs/snapshots/* /home/publisher/.fhir/packages
+echo "home publisher .fhir packages"
+ls -al /home/publisher/.fhir/packages
