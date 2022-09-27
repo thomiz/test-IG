f44d550ed83a198095593d5be019f306dcfaca16
thomiz <thomasrosenlund@gmail.com>
2022-09-27 19:59:53 +0200

    bugfix the install snapshot script

--- install-no-basis-snapshots.sh
+++ install-no-basis-snapshots.sh
@@ -5,7 +5,7 @@ echo "NPM install fhir no-basis212beta"
 npm install /github/workspace/igs/snapshots/hl7.fhir.no.basis-2.1.2-beta.tgz
 echo "Copy no-basis"
 mkdir -p /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
-cp -r /home/publisher/node_modules/hl7.fhir.no.basis/* /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
+cp -r /gihub/workspace/node_modules/hl7.fhir.no.basis/* /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
 echo "Copy a bogus ImplementationGuide resource instance to the package catalog for SUSHI to run (it needs to have the correct version in version element)"
  cp ig/igs/snapshots/ImplementationGuide-hl7.fhir.no.basis.json /root/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package/
 echo "get latest publisher"
