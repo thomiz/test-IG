40bdd225438069cef21e4b2ea1292636b9c8a47e
thomiz <thomasrosenlund@gmail.com>
2022-09-28 07:12:38 +0200

    Testing without the IG copy step of the shell script

--- docker.sh
+++ docker.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-echo "Test"
-ls -al
-pwd
-ls -al /github/workspace/igs/snapshots
-mkdir -p /github/workspace/.fhir/packages
-ls -al /github/workspace/.fhir/packages
-echo "make directory home publisher fhir packages"
-mkdir -p /home/publisher/.fhir/packages
-echo "Copy"
-cp -r /github/workspace/igs/snapshots/* /home/publisher/.fhir/packages
-echo "home publisher .fhir packages"
-ls -al /home/publisher/.fhir/packages
+echo "NPM install fhir r4 core"
+npm install /github/workspace/igs/snapshots/hl7.fhir.r4.core.tgz
+echo "NPM install fhir no-basis212beta"
+npm install /github/workspace/igs/snapshots/hl7.fhir.no.basis-2.1.2-beta.tgz
+echo "List node_modules directory"
+ls -al /github/workspace/node_modules
+echo "List node_modules hl7.fhir.no.basis directory"
+ls -al /github/workspace/node_modules/hl7.fhir.no.basis
+echo "Make home publisher .fhir packages no-basis"
+mkdir -p /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
+ls -al /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
+echo "Copy no-basis"
+cp -r /github/workspace/node_modules/hl7.fhir.no.basis/* /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
+ls -al /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
+#echo "Copy a bogus ImplementationGuide resource instance to the package catalog for SUSHI to run (it needs to have the correct version in version element)"
+#cp igs/snapshots/ImplementationGuide-hl7.fhir.no.basis.json /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
+echo "get latest publisher"
+curl -L https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar -o ./input-cache/publisher.jar --create-dirs
 echo "Run publisher: java -jar publisher.jar publisher -ig igs/$1/ig.ini"
-java -jar publisher.jar publisher -ig igs/$1/ig.ini
+java -jar ./input-cache/publisher.jar publisher -ig igs/$1/ig.ini
