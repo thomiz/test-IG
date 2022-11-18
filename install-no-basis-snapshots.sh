2296ce79353fdebe39cb103e68386aea556d4c20
thomiz <thomasrosenlund@gmail.com>
2022-11-18 13:50:31 +0100

    Update dependency to no.basis-2.1.2 from delta version

--- install-no-basis-snapshots.sh
+++ install-no-basis-snapshots.sh
@@ -2,17 +2,17 @@
 echo "NPM install fhir r4 core 4.0.1 from package registry"
 npm --registry https://packages.simplifier.net install hl7.fhir.r4.core@4.0.1
 echo "NPM install fhir no-basis212beta from local tgz"
-npm install /github/workspace/igs/snapshots/hl7.fhir.no.basis-2.1.2-delta.tgz
+npm install /github/workspace/igs/snapshots/hl7.fhir.no.basis-2.1.2.tgz
 #echo "List node_modules directory"
 #ls -al /github/workspace/node_modules
 #echo "List node_modules hl7.fhir.no.basis directory"
 #ls -al /github/workspace/node_modules/hl7.fhir.no.basis
 echo "Make home publisher .fhir packages no-basis"
-mkdir -p /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-delta/package
-ls -al /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-delta/package
+mkdir -p /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2/package
+ls -al /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2/package
 echo "Copy no-basis to /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package"
-cp -r /github/workspace/node_modules/hl7.fhir.no.basis/* /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-delta/package
-ls -al /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-delta/package
+cp -r /github/workspace/node_modules/hl7.fhir.no.basis/* /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2/package
+ls -al /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2/package
 #echo "Copy a bogus ImplementationGuide resource instance to the package catalog for SUSHI to run (it needs to have the correct version in version element)"
 #cp igs/snapshots/ImplementationGuide-hl7.fhir.no.basis.json /home/publisher/.fhir/packages/hl7.fhir.no.basis#2.1.2-beta/package
 echo "get latest publisher"
